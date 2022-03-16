d20572ed9c19bb49225adf8e0ba956c6ffaf1e8c
Mohammad Yusuf Beg <82448349+Yusuf-max552@users.noreply.github.com>
2022-03-16 12:35:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,4 +3,17 @@ function setup(){
     video.size(550, 500);
     canvas = createCanvas(550, 500);
     canvas.position(560, 150);
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function draw(){
+    background('#880000');
+}
+function modelLoaded(){
+    console.log('PoseNet is Initialized');
+}
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+    }
 }
